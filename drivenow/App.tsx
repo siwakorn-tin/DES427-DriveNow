b080be2b07331d926f40c2ebf547432cf4c074d6
siwakorn-tin <6422770725@g.siit.tu.ac.th>
2024-11-20 22:13:47 +0700

    add tamagui

--- drivenow/App.tsx
+++ drivenow/App.tsx
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text } from "react-native";
+import { TamaguiProvider, View } from "@tamagui/core";
+import appConfig from "./tamagui.config";
+import { useFonts } from "expo-font";
+import React from "react";
+import { Button, Input, TextArea, XStack, YStack } from "tamagui";
+import type { SizeTokens } from "tamagui";
 
 export default function App() {
+  const [loaded] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
+  if (!loaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TamaguiProvider config={appConfig}>
+      <View width={200} height={200} backgroundColor="$background" />
+      <YStack
+        width={200}
+        minHeight={250}
+        overflow="hidden"
+        space="$2"
+        margin="$3"
+        padding="$2"
+      >
+        <InputDemo size="$2" />
+        <InputDemo size="$3" />
+        <InputDemo size="$4" />
+        <TextArea placeholder="Enter your details..." />
+      </YStack>
+    </TamaguiProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function InputDemo(props: { size: SizeTokens }) {
+  return (
+    <XStack alignItems="center" space="$2">
+      <Input flex={1} size={props.size} placeholder={`Size ${props.size}...`} />
+      <Button size={props.size}>Go</Button>
+    </XStack>
+  );
+}
